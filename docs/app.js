b4f8aeac2f0d61b188014f6109e6d4c551d42e20
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-12 17:43:20 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -2,6 +2,31 @@ d3.json("samples.json", function(data) {
     console.log(data);
 });
 
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+function updatePlotly() {
+  var dropdownMenu = d3.select("#optionChanged(this.value)");
+  var dataset = dropdownMenu.property("value");
+
+  var data = [];
+
+  if (dataset == '940') {
+      data = samples.id[0];
+  }
+  else if (dataset == '941') {
+      data = samples.id[1];
+  }
+  else if (dataset == '943') {
+      data = samples.id[2];
+  }
+  updatePlotly(data);
+
+}
+
+function updatePlotly(newdata) {
+    Plotly.restyle("bar", "values", [newdata]);
+}
+
 function filterOTU(otu) {
     data.sample_values.slice(0,9);
     return otu.sample_values;
@@ -29,29 +54,4 @@ var layout = {
   };
   
 Plotly.newPlot("bar", filtered_data, layout);
-
-d3.selectAll("#selDataset").on("change", updatePlotly);
-
-function updatePlotly() {
-  var dropdownMenu = d3.select("#optionChanged(this.value)");
-  var dataset = dropdownMenu.property("value");
-
-  var data = [];
-
-  if (dataset == '940') {
-      data = samples.id[0];
-  }
-  else if (dataset == '941') {
-      data = samples.id[1];
-  }
-  else if (dataset == '943') {
-      data = samples.id[2];
-  }
-  updatePlotly(data);
-
-}
-
-function updatePlotly(newdata) {
-    Plotly.restyle("bar", "values", [newdata]);
-}
 
