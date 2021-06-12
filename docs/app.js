0cea5188a6bee55f24ed6161cfc0ddbf33710851
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-12 17:39:33 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -5,17 +5,53 @@ d3.json("samples.json", function(data) {
 function filterOTU(otu) {
     data.sample_values.slice(0,9);
     return otu.sample_values;
-}
+  }
 
 var filteredOTUs = data.filter(filterOTU);
-
-console.log(filteredOTUs);
     
 var id_name = filteredOTUs.map(otu => otu.otu_ids);
 
-console.log(id_name);
-
 var otu_values = filteredOTUs.map(otu => otu.sample_values);
 
-console.log(otu_values);
+var trace = {
+    x: otu_values,
+    y: id_name,
+    type: "bar",
+    hoverinfo: otu_labels
+  };
+  
+var filtered_data = [trace];
+  
+var layout = {
+    title: "Top Ten OTUs for a Sample Person",
+    xaxis: { title: "OTU Values" },
+    yaxis: { title: "OTU ID"}
+  };
+  
+Plotly.newPlot("bar", filtered_data, layout);
+
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+function updatePlotly() {
+  var dropdownMenu = d3.select("#optionChanged(this.value)");
+  var dataset = dropdownMenu.property("value");
+
+  var data = [];
+
+  if (dataset == '940') {
+      data = samples.id[0];
+  }
+  else if (dataset == '941') {
+      data = samples.id[1];
+  }
+  else if (dataset == '943') {
+      data = samples.id[2];
+  }
+  updatePlotly(data);
+
+}
+
+function updatePlotly(newdata) {
+    Plotly.restyle("bar", "values", [newdata]);
+}
 
