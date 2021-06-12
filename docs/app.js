1b56a99824b88e5f30822cb9bfb5f4fceb8dfe6b
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-12 17:34:52 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -2,3 +2,20 @@ d3.json("samples.json", function(data) {
     console.log(data);
 });
 
+function filterOTU(otu) {
+    data.sample_values.slice(0,9);
+    return otu.sample_values;
+}
+
+var filteredOTUs = data.filter(filterOTU);
+
+console.log(filteredOTUs);
+    
+var id_name = filteredOTUs.map(otu => otu.otu_ids);
+
+console.log(id_name);
+
+var otu_values = filteredOTUs.map(otu => otu.sample_values);
+
+console.log(otu_values);
+
