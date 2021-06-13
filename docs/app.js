351ffcbf11feb3d44b38f75eefcc3e866329786a
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-13 12:05:56 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,57 +1,34 @@
-d3.json("samples.json", function(data) {
-    console.log(data);
-});
-
-d3.selectAll("#selDataset").on("change", updatePlotly);
-
-function updatePlotly() {
-  var dropdownMenu = d3.select("#optionChanged(this.value)");
-  var dataset = dropdownMenu.property("value");
-
-  var data = [];
-
-  if (dataset == '940') {
-      data = samples.id[0];
-  }
-  else if (dataset == '941') {
-      data = samples.id[1];
-  }
-  else if (dataset == '943') {
-      data = samples.id[2];
-  }
-  updatePlotly(data);
-
-}
-
-function updatePlotly(newdata) {
-    Plotly.restyle("bar", "values", [newdata]);
-}
-
-function filterOTU(otu) {
-    data.sample_values.slice(0,9);
-    return otu.sample_values;
-  }
-
-var filteredOTUs = data.filter(filterOTU);
-    
-var id_name = filteredOTUs.map(otu => otu.otu_ids);
-
-var otu_values = filteredOTUs.map(otu => otu.sample_values);
-
-var trace = {
-    x: otu_values,
-    y: id_name,
-    type: "bar",
-    hoverinfo: otu_labels
-  };
+d3.json("data/samples.json").then((importedData) => {
+    var data = importedData;
   
-var filtered_data = [trace];
+    data.sort(function(a, b) {
+      return parseFloat(b.sample_values) - parseFloat(a.sample_values);
+    });
   
-var layout = {
-    title: "Top Ten OTUs for a Sample Person",
-    xaxis: { title: "OTU Values" },
-    yaxis: { title: "OTU ID"}
-  };
+    data = data.slice(0, 10);
   
-Plotly.newPlot("bar", filtered_data, layout);
-
+    data = data.reverse();
+  
+    var trace1 = {
+      x: data.map(row => row.sample_values),
+      y: data.map(row => row.otu_ids),
+      text: data.map(row => row.otu_ids),
+      type: "bar",
+      orientation: "h"
+    };
+  
+    var chartData = [trace1];
+  
+    var layout = {
+      title: "Top Ten OTU per Person",
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+      }
+    };
+  
+    Plotly.newPlot("bar", chartData, layout);
+  });
+  
